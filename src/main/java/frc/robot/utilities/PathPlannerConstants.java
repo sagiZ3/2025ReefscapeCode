138e67fff3c2813571662d395a706df260fb741b
itsWihy <devwihy@gmail.com>
2025-01-12 12:45:58 +0200

    Style: moved PathPlannerConstants to their own class instead of cluttering swerve.

---
+++ src/main/java/frc/robot/utilities/PathPlannerConstants.java
@@ -0,0 +1,56 @@
+package frc.robot.utilities;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.FollowPathCommand;
+import com.pathplanner.lib.commands.PathfindingCommand;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.pathfinding.Pathfinding;
+import frc.lib.util.LocalADStarAK;
+import frc.lib.util.mirrorable.Mirrorable;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+import static frc.robot.RobotContainer.POSE_ESTIMATOR;
+import static frc.robot.RobotContainer.SWERVE;
+
+public class PathPlannerConstants {
+    public static final RobotConfig ROBOT_CONFIG = getRobotConfig();
+
+    public static final PPHolonomicDriveController PATHPLANNER_PID_CONSTANTS = new PPHolonomicDriveController(
+            new PIDConstants(2, 0.0, 0.2),
+            new PIDConstants(1, 0.0, 0.05)
+    );
+
+    public static void initializePathPlanner() {
+        Pathfinding.setPathfinder(new LocalADStarAK());
+
+        configurePathPlanner();
+
+        FollowPathCommand.warmupCommand().schedule();
+        PathfindingCommand.warmupCommand().schedule();
+    }
+
+    private static void configurePathPlanner() {
+        AutoBuilder.configure(
+                POSE_ESTIMATOR::getCurrentPose,
+                POSE_ESTIMATOR::resetPose,
+                SWERVE::getRobotRelativeVelocity,
+                SWERVE::driveRobotRelative,
+                PATHPLANNER_PID_CONSTANTS,
+                ROBOT_CONFIG,
+                Mirrorable::isRedAlliance,
+                SWERVE
+        );
+    }
+
+    private static RobotConfig getRobotConfig() {
+        try {
+            return RobotConfig.fromGUISettings();
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
