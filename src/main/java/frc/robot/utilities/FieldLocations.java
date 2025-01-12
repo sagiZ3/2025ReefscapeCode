4b55e19d16187a834f2a173de0b25d68ccd12f2c
biqsweet <ranranlevyy@gmail.com>
2025-01-13 00:51:41 +0200

    Fix: pathfinder rotates to the correct angle for each feeder

--- src/main/java/frc/robot/utilities/FieldLocations.java
+++ src/main/java/frc/robot/utilities/FieldLocations.java
@@ -6,7 +6,7 @@ import edu.wpi.first.math.geometry.Translation3d;
 
 public class FieldLocations {
     public static final Pose3d BLUE_TOP_FEEDER = new Pose3d(new Translation3d(0.84319, 7.41395, 0.9525),
-            new Rotation3d(0, 0, Math.toRadians(-54)));
+            new Rotation3d(0, 0, Math.toRadians(126)));
     public static final Pose3d BLUE_BOTTOM_FEEDER = new Pose3d(new Translation3d(0.84319, 0.65078, 0.9525),
-            new Rotation3d(0, 0, Math.toRadians(54)));
+            new Rotation3d(0, 0, Math.toRadians(234)));
 }
