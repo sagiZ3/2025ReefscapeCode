9e30ae3d1b67d1c646247b70aa36ec6ed93f4e9e
biqsweet <ranranlevyy@gmail.com>
2025-01-14 21:25:50 +0200

    Fix: changed feeder positions to be next to feeder

--- src/main/java/frc/robot/utilities/FieldLocations.java
+++ src/main/java/frc/robot/utilities/FieldLocations.java
@@ -5,8 +5,8 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 
 public class FieldLocations {
-    public static final Pose3d BLUE_TOP_FEEDER = new Pose3d(new Translation3d(0.84319, 7.41395, 0.9525),
+    public static final Pose3d BLUE_TOP_FEEDER = new Pose3d(new Translation3d(1.212, 7.41395, 0.9525),
             new Rotation3d(0, 0, Math.toRadians(126)));
-    public static final Pose3d BLUE_BOTTOM_FEEDER = new Pose3d(new Translation3d(0.84319, 0.65078, 0.9525),
+    public static final Pose3d BLUE_BOTTOM_FEEDER = new Pose3d(new Translation3d(1.212, 1.151, 0.9525),
             new Rotation3d(0, 0, Math.toRadians(234)));
 }
