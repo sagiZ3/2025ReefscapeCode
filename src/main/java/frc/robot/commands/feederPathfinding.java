b22166e4094696d76afa57db06449823e6b66b80
biqsweet <ranranlevyy@gmail.com>
2025-01-15 01:07:29 +0200

    Fix: added a small pre-programmed path that goes from next to feeder, to feeder

--- src/main/java/frc/robot/commands/feederPathfinding.java
+++ src/main/java/frc/robot/commands/feederPathfinding.java
@@ -1,19 +1,28 @@
 package frc.robot.commands;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.lib.util.flippable.FlippablePose2d;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 import static frc.robot.RobotContainer.POSE_ESTIMATOR;
-import static frc.robot.utilities.FieldLocations.BLUE_BOTTOM_FEEDER;
-import static frc.robot.utilities.FieldLocations.BLUE_TOP_FEEDER;
 import static frc.robot.utilities.PathPlannerConstants.PATHPLANNER_CONSTRAINTS;
 
 public class feederPathfinding {
     public static void goToClosestFeeder(Trigger button) {
-        Trigger closetFeeder = new Trigger(() -> POSE_ESTIMATOR.getCurrentPose().getY() - 4 < 0);
+        Trigger closetFeeder = new Trigger(() -> POSE_ESTIMATOR.getCurrentPose().getY() - 4 > 0);
 
-        button.and(closetFeeder).whileTrue(AutoBuilder.pathfindToPose(new FlippablePose2d(BLUE_BOTTOM_FEEDER.toPose2d(), true).get(), PATHPLANNER_CONSTRAINTS));
-        button.and(closetFeeder.negate()).whileTrue(AutoBuilder.pathfindToPose(new FlippablePose2d(BLUE_TOP_FEEDER.toPose2d(), true).get(), PATHPLANNER_CONSTRAINTS));
+        try {
+            button.and(closetFeeder).whileTrue(AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("nextToTopFeeder").mirrorPath(), PATHPLANNER_CONSTRAINTS));
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException("Failed to load path", e);
+        }
+        try {
+            button.and(closetFeeder.negate()).whileTrue(AutoBuilder.pathfindThenFollowPath(PathPlannerPath.fromPathFile("nextToBottomFeeder").mirrorPath(), PATHPLANNER_CONSTRAINTS));
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException("Failed to load path", e);
+        }
     }
 }
